d884115436816b3621859998160226a9b4775d61
mariano.palliser <mariano.palliser@SM2baleares.es>
2017-06-16 13:04:45 +0200

    first commit

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,13 @@
+/**
+ * Created by mariano.palliser on 15/06/2017.
+ */
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+  styleUrls: ['./server.component.css']
+})
+export class ServerComponent {
+
+}
